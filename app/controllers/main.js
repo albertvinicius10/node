8b09fbb9c4cf908aad751da857ea9f348bc98b66
albertvinicius10 <albertvinicius10@gmail.com>
2023-02-09 14:33:37 -0400

    first

---
+++ app/controllers/main.js
@@ -0,0 +1,23 @@
+function index(req, res){
+    res.render("main/index",{
+        titulo: "Professores do IComp",
+        mostraTitulo: true,
+        professores: [
+            {nome: "David Fernandes", sala: 1638},
+            {nome: "Horácio Fernandes", sala: 1639},
+            {nome: "Edleno Moura", sala: 1636},
+            {nome: "Leandro galvão", sala: 1626},
+        ]
+    });
+}
+
+
+function sobre(req, res){
+    const conteudo = 'Página sobre a aplicação';
+    res.render('main/sobre',{
+        conteudo: conteudo
+    });
+};
+
+
+module.exports = { index, sobre}
